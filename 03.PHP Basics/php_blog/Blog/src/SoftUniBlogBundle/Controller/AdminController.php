a23dff97fbb807c9bcdc8bcc4b49ce5fcc753a48
Stanislav Kozlovski <familyguyuser192@windowslive.com>
2016-11-03 12:54:36 +0200

    Admins can now delete user profiles

--- 03.PHP Basics/php_blog/Blog/src/SoftUniBlogBundle/Controller/AdminController.php
+++ 03.PHP Basics/php_blog/Blog/src/SoftUniBlogBundle/Controller/AdminController.php
@@ -26,6 +26,30 @@ class AdminController extends Controller
         $users = $this->getDoctrine()->getRepository(User::class)->findAll();
         return $this->render('admin/users_list.html.twig', ['users'=> $users]);
     }
+
+    /**
+     * @Route("/admin/delete_user/{id}", name="user_delete")
+     * @param $id
+     * @return \Symfony\Component\HttpFoundation\RedirectResponse|\Symfony\Component\HttpFoundation\Response
+     */
+    public function deleteUserAction($id)
+    {
+
+
+        // ...you heartless bastard...
+        $user = $this->getDoctrine()->getRepository(User::class)->findOneBy(['id' => $id]);
+        if ($this->getUser() === null or !$this->getUser()->isAdmin() or !$user)
+        {
+            return $this->redirectToRoute('blog_index');
+        }
+        $em = $this->getDoctrine()->getManager();
+        $em->remove($user);
+        $em->flush();
+
+        return $this->render('admin/users_list.html.twig',
+            ['message' => true,
+                'success_message' => 'Sucessfully deleted user '.$user->getUsername()]);
+    }
     public function indexAction($name)
     {
         return $this->render('', array('name' => $name));
