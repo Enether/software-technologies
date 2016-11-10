c2d5d1cb0570a69ff61b9a4c161ef8c368c68e76
Stanislav Kozlovski <familyguyuser192@windowslive.com>
2016-11-10 15:43:30 +0200

    Added the display of articles on the main page

--- 04.JavaScript Blog/controllers/home-controller.js
+++ 04.JavaScript Blog/controllers/home-controller.js
@@ -1,5 +1,11 @@
+const Article = require('mongoose').model('Article')
+
 module.exports = {
   index: (req, res) => {
-    res.render('layout')
+    Article
+      .find()
+      .then((articles) => {
+        res.render('home/index', {articles: articles})
+      })
   }
 }
