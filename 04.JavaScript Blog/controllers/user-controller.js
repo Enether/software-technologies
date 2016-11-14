1b0c96f377db24edd24a0905b9517f41eaf68932
Stanislav Kozlovski <familyguyuser192@windowslive.com>
2016-11-14 13:20:35 +0200

    Fixed a problem where the register page would not render because of a lack of the registration arguments object

--- 04.JavaScript Blog/controllers/user-controller.js
+++ 04.JavaScript Blog/controllers/user-controller.js
@@ -3,7 +3,7 @@ const encryption = require('./../utilities/encryption')
 
 module.exports = {
   registerGet: (req, res) => {
-    res.render('user/register', { user: {} })
+    res.render('user/register', { user: {}, regArgs: {} })
   },
 
   registerPost: (req, res) => {
