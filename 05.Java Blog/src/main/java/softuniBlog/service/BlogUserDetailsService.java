14b0acee770c3a9293f943605b7d0652d4dc52d0
Stanislav Kozlovski <familyguyuser192@windowslive.com>
2016-11-22 13:26:49 +0200

    Added WebSecurityConfig

--- 05.Java Blog/src/main/java/softuniBlog/service/BlogUserDetailsService.java
+++ 05.Java Blog/src/main/java/softuniBlog/service/BlogUserDetailsService.java
@@ -2,6 +2,7 @@ package softuniBlog.service;
 
 import org.springframework.security.core.GrantedAuthority;
 import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.stereotype.Service;
